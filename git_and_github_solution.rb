f82e5a4c8fcf58700046f4b3df66663418b0684f
callaway <kcallawayworks@gmail.com>
2019-01-10 09:36:56 -0700

    adds today's homework

--- git_and_github_solution.rb
+++ git_and_github_solution.rb
@@ -1,7 +1,10 @@
-# Paste a link to your github repository below:
+# # Paste a link to your github repository below:
 
-https://github.com/kcallawayworks/hello-world
+# https://github.com/kcallawayworks/hello-world
 
-# What questions do you have about what we've learned so far using git and github?:
+# # What questions do you have about what we've learned so far using git and github?:
 
-puts none at this time
+# Testing
+a = 1
+puts "this is a test"
+p a
